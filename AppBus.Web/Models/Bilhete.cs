6d1ec8be4abb1800ebb4f356af036e9ab7f1d9da
emanuellex <rm88684@fiap.com.br>
2022-11-06 19:49:28 -0300

    Update Bilhete

--- AppBus.Web/Models/Bilhete.cs
+++ AppBus.Web/Models/Bilhete.cs
@@ -1,6 +1,33 @@
-﻿namespace AppBus.Web.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AppBus.Web.Models
 {
     public class Bilhete
     {
+        [HiddenInput, Column("Id")]
+        public string? BilheteID { get; set; }
+
+
+        [Required, MaxLength(9), Column("NR_BILHETE"), Display(Name = "Número do Bilhete")]
+        public string? NumeroBilhete { get; set; }
+
+        [Required, Column("NM_TITULAR"), Display(Name = "Nome do Titular")]
+        public string? NomeTitular { get; set; }
+
+        [Column("VL_VALOR")]
+        public decimal Valor { get; set; }
+
+        [Column("DS_TIPO_BILHETE"), Display(Name = "Tipo")]
+        public TipoBilhete TipoBilhete { get; set; }
+
+        public Usuario Usuario { get; set; }
+        public int UsuarioId { get; set; }
+    }
+    public enum TipoBilhete
+    {
+        Comum, Estudante, Semanal, Mensal, Diario, Tranporte
     }
 }
