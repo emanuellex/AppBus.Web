32f0e7336877769d6dc2af5c6f93e3285e1ffef2
emanuellex <rm88684@fiap.com.br>
2022-11-06 19:34:38 -0300

    Update CartaoCredito

--- AppBus.Web/Models/Avaliacao.cs
+++ AppBus.Web/Models/Avaliacao.cs
@@ -1,6 +1,20 @@
-﻿namespace AppBus.Web.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AppBus.Web.Models
 {
-    public class Avaliacao
-    {
+    public class Avaliacao { 
+
+
+    [Required, MaxLength(500), Column("NR_AVALIACAO")]
+    public string? Numero { get; set; }
+
+
+    [Required, MaxLength(500), Column("DC_COMENTARIO")]
+        public string? Comentario { get; set; }
+
+
     }
 }
--- AppBus.Web/Models/CartaoCredito.cs
+++ AppBus.Web/Models/CartaoCredito.cs
@@ -1,6 +1,42 @@
-﻿namespace AppBus.Web.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppBus.Web.Models
 {
     public class CartaoCredito
     {
+        [HiddenInput, Column("Id")]
+        public string? CartaoCreditoID { get; set; }
+
+
+        [Required, MaxLength(16), Column("NR_Cartao")]
+        public string? NumeroCartao { get; set; }
+
+        [Required,Column("NM_TITULAR")]
+        public string? NomeTitular { get; set; }
+
+
+        [MaxLength(11), Display(Name = "CPF"), Required, Column("NR_CPF")]
+        public string? Documento { get; set; }
+
+        [MaxLength(3), Display(Name = "CVV"), Required, Column("NR_CVV")]
+        public string? CVV { get; set; }
+
+        [MaxLength(3), Display(Name = "Validade"), Required, Column("DT_VALIDADE")]
+        public DateOnly Validade { get; set; }
+
+        [Required, Column("DS_Bandeira")]
+        public Bandeira Bandeira { get; set; }
+
+        public Usuario Usuario { get; set; }
+        public int UsuarioId { get; set; }
+
+
+    }
+
+    public enum Bandeira
+    {
+        Elo, Visa, Mastercard
     }
 }
